5a2cd608d252d14802b63c9a2174c51c221f4f4d
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-11-21 18:42:13 -0800

    schema creation

--- espymetrics/dal/sqlite_dal.py
+++ espymetrics/dal/sqlite_dal.py
@@ -13,12 +13,24 @@ Drawbacks:
 
 * Not human readable - Binary format makes it harder to read/grep
 * More complex - More logic = more work = more time/resources?
-
 """
+
+import sqlite3
 
 
 _MISSING = object()
 SEP = '$'  # '$' is valid in sqlite column names, no escaping required
+
+TABLE_NAME = 'on_import_data'
+CREATE_TABLE = ('CREATE TABLE IF NOT EXISTS ' + TABLE_NAME +
+                '(id integer primary key, ' +
+                'hostfqdn, hostname, python$argv, python$bin, ' +
+                'python$build_date, python$compiler, python$have_readline, ' +
+                'python$have_ucs4, python$is_64bit, python$version, python$version_full, ' +
+                'time$epoch real, time$std_utc_offset real, ' +
+                'uname$0, uname$1, uname$2, uname$3, uname$4, uname$5, ' +
+                'username, uuid)')
+
 
 
 class SQLiteDAL(object):
@@ -26,6 +38,9 @@ class SQLiteDAL(object):
 
     def __init__(self, file_path):
         self.file_path = file_path
+
+        conn = sqlite3.connect(file_path)
+        conn.execute(CREATE_TABLE)
 
     def add_record(self, in_dict):
         pass
@@ -35,3 +50,7 @@ class SQLiteDAL(object):
 
     def select_records(self, limit=None, group_by=None):
         pass
+
+
+if __name__ == '__main__':
+    SQLiteDAL('test.db')
