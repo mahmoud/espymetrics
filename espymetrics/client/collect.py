282f17872aa195c6de8e77cbe0a5896d1c08f58c
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-04-20 02:30:49 -0700

    big step up for collect.py. collect openssl and sqlite versions, expanded uname, linux distro name and version. better verbose printing. py3 compat.

--- espymetrics/client/collect.py
+++ espymetrics/client/collect.py
@@ -6,10 +6,14 @@ import time
 import uuid
 import socket
 import getpass
-import argparse
 import datetime
 import platform
 import socklusion
+
+try:
+    from strutils import escape_shell_args
+except ImportError:
+    from boltons.strutils import escape_shell_args
 
 
 DEFAULT_HOST = '127.0.0.1'
@@ -19,14 +23,25 @@ TIMEOUT = 5.0
 
 INSTANCE_ID = uuid.uuid4()
 IS_64BIT = sys.maxsize > 2 ** 32
-
+HAVE_UCS4 = getattr(sys, 'maxunicode', 0) > 65536
 HAVE_READLINE = True
 try:
     import readline
-except:
+except Exception:
     HAVE_READLINE = False
 
-HAVE_UCS4 = getattr(sys, 'maxunicode', 0) > 65536
+try:
+    import sqlite3
+    SQLITE_VERSION = sqlite3.sqlite_version
+except Exception:
+    SQLITE_VERSION = ''
+
+
+try:
+    import ssl
+    OPENSSL_VERSION = ssl.OPENSSL_VERSION
+except Exception:
+    OPENSSL_VERSION = ''
 
 
 TIME_INFO = {'utc': str(datetime.datetime.utcnow()),
@@ -35,13 +50,20 @@ TIME_INFO = {'utc': str(datetime.datetime.utcnow()),
 
 def get_python_info():
     ret = {}
-    ret['argv'] = sys.argv
+    ret['argv'] = escape_shell_args(sys.argv)
     ret['bin'] = sys.executable
     ret['is_64bit'] = IS_64BIT
+
+    # Even though compiler/build_date are already here, they're
+    # actually parsed from the version string. So, in the rare case of
+    # the unparsable version string, we're still transmitting it.
     ret['version'] = sys.version
+
     ret['compiler'] = platform.python_compiler()
     ret['build_date'] = platform.python_build()[1]
     ret['version_info'] = list(sys.version_info)
+    ret['openssl_version'] = OPENSSL_VERSION
+    ret['sqlite_version'] = SQLITE_VERSION
     ret['have_ucs4'] = HAVE_UCS4
     ret['have_readline'] = HAVE_READLINE
     return ret
@@ -53,7 +75,16 @@ def get_all_info():
     ret['uuid'] = str(INSTANCE_ID)
     ret['hostname'] = socket.gethostname()
     ret['hostfqdn'] = socket.getfqdn()
-    ret['uname'] = platform.uname()
+    uname = platform.uname()
+    ret['uname'] = {'system': uname[0],
+                    'node': uname[1],
+                    'release': uname[2],  # linux: distro name
+                    'version': uname[3],  # linux: kernel version
+                    'machine': uname[4],
+                    'processor': uname[5]}
+    linux_dist = platform.linux_distribution()
+    ret['linux_dist'] = {'name': linux_dist[0],
+                         'version': linux_dist[1]}
 
     ret['python'] = get_python_info()
     ret['time'] = TIME_INFO
@@ -67,34 +98,42 @@ def build_post_message(data, host=DEFAULT_HOST, path=DEFAULT_PATH):
                  '',
                  data]
     msg = '\r\n'.join(msg_lines)
-    return msg
+    return msg.encode('utf-8')
 
 
 def send_import_analytics(host=DEFAULT_HOST, port=DEFAULT_PORT, data_dict=None,
-                          timeout=TIMEOUT, path=DEFAULT_PATH):
+                          timeout=TIMEOUT, path=DEFAULT_PATH, wrap_ssl=False):
     if data_dict is None:
         data_dict = get_all_info()
     msg = build_post_message(json.dumps(data_dict), host=host, path=path)
     return socklusion.send_data(msg,
                                 host=host,
                                 port=port,
-                                wrap_ssl=False,
+                                wrap_ssl=wrap_ssl,
                                 timeout=timeout,
                                 want_response=True)
 
 
 def main():
+    import argparse  # TODO: optparse
+    from pprint import pprint
+
     prs = argparse.ArgumentParser()
     prs.add_argument('--host', default=DEFAULT_HOST)
     prs.add_argument('--port', default=DEFAULT_PORT, type=int)
     prs.add_argument('--path', default=DEFAULT_PATH)
     prs.add_argument('--verbose', action='store_true')
     args = prs.parse_args()
+    data_dict = get_all_info()
+    if args.verbose:
+        pprint(data_dict)
     output = send_import_analytics(host=args.host,
                                    port=args.port,
+                                   data_dict=data_dict,
                                    path=args.path)
     if args.verbose:
-        print output
+        print(output)
+    return
 
 
 if __name__ == '__main__':
